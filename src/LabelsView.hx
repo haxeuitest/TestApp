6989d87613e1b025b4c90067173ee345ad96a5a3
Joe Williamson <joe.williamson@realitycharting.com>
2024-12-11 17:49:32 -0800

    Fixing Captial issue

--- src/LabelsView.hx
+++ src/LabelsView.hx
@@ -2,7 +2,7 @@ package ;
 
 import haxe.ui.containers.VBox;
 
-@:build(haxe.ui.macros.ComponentMacros.build("labelsView.xml"))
+@:build(haxe.ui.macros.ComponentMacros.build("LabelsView.xml"))
 //@:xml("<vbox></vbox>")
 @:keep @:expose
 class LabelsView extends VBox {
